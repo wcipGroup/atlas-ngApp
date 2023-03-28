a5b973940285259286602310121a427493f996f6
soteg <86359025+soteg@users.noreply.github.com>
2023-03-28 10:54:46 +0300

    Update predictions.component.ts

--- src/app/predictions/predictions.component.ts
+++ src/app/predictions/predictions.component.ts
@@ -54,7 +54,8 @@ export class PredictionsComponent implements OnInit{
         this.conductivity_values = [];
         //this.device_data = this.device_data.slice(6)
         this.device_data.map(data=>{
-            this.date_values.push(data["date"])
+            let date = moment(data["date"]).format('DD/MM/YYYY, HH:mm:ss');
+            this.date_values.push(date);
             var sensor_value = data["SensorsValue"]
             this.temperature_values.push(sensor_value.find(sensor=>sensor.sensorId==1).value)
             this.ph_values.push(sensor_value.find(sensor=>sensor.sensorId==2).value)
